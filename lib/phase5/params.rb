82551d2e4a3238c2f756dd637214f902b8d5b78f
Zak Niazi <zniazi1029@gmail.com>
2014-08-06 10:41:40 -0400

    Complete parsing of request query strings.

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -1,5 +1,6 @@
 require 'uri'
 require 'pry-debugger'
+require 'webrick'
 
 module Phase5
   class Params
@@ -23,7 +24,8 @@ module Phase5
 
     class AttributeNotFoundError < ArgumentError; end;
 
-    private
+    # private
+
     # this should return deeply nested hash
     # argument format
     # user[address][street]=main&user[address][zip]=89436
@@ -33,14 +35,23 @@ module Phase5
       return nil if www_encoded_form.nil?
 
       URI.decode_www_form(www_encoded_form).each do |el|
-        nested_keys = parse_key(el[0]).reverse!
-        @params = { nested_keys.shift => el[1] }
-        until nested_keys.empty?
-          @params = { nested_keys.shift => @params }
+        if parse_key(el[0]).length > 1
+          nested_keys = parse_key(el[0]).reverse!
+          @params = { nested_keys.shift => el[1] }
+          @params = { nested_keys.shift => @params } until nested_keys.empty?
+        else
+          @params[el[0]] = el[1]
         end
-
-        @params
       end
+      # URI.decode_www_form(www_encoded_form).each do |el|
+#         nested_keys = parse_key(el[0]).reverse!
+#         @params = { nested_keys.shift => el[1] }
+#         until nested_keys.empty?
+#           @params = { nested_keys.shift => @params }
+#         end
+#
+#         @params
+      # end
     end
 
     # this should return an array
@@ -51,5 +62,7 @@ module Phase5
   end
 end
 
+# puts Phase5::Params.new(WEBrick::HTTPRequest.new(Logger: nil)).parse_www_encoded_form("user[address][street]=main&user[address][zip]=89436")
+# p Phase5::Params.new(WEBrick::HTTPRequest.new(Logger: nil)).parse_key("user_address_street")
 
 
